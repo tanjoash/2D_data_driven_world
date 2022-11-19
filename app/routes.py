8cf12f3afc43ed9106de8245e46c944bbf1fa6e3
Joash Tan <78843046+tanjoash@users.noreply.github.com>
2022-11-19 18:39:56 +0800

    Added Report page
    Added styling and report page and navigation fix

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,14 @@ from flask import render_template
 from app import application
 
 @application.route('/')
-def base():
-    return render_template('base.html', title='2D Project')
+@application.route('/index')
+def index():
+    return render_template('index.html')
 
+@application.route('/report')
+def report():
+    return render_template('report.html')
+
+@application.route('/graph')
+def graph():
+    return render_template('graph.html')
